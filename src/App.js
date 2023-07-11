aab030120e447b914ef541592c9f202f9dd92d45
hyein <rin1401@naver.com>
2023-07-11 16:58:58 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Main from "./Components/Main";
+import { Routes, Route } from "react-router-dom";
+import { styled } from "styled-components";
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-color: #ddd;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
+    </Container>
   );
 }
 
---
+++ src/Components/Main.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { styled } from "styled-components";
+
+const Main = () => {
+  return <div>mainpage</div>;
+};
+
+export default Main;
