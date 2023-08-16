73ebf238216fd1513456b8eefce503c13de95cfc
hyein <rin1401@naver.com>
2023-08-16 20:12:06 +0900

    edit mobile css

--- src/Pages/WorkVeiw.js
+++ src/Pages/WorkVeiw.js
@@ -168,7 +168,7 @@ const Bottomdiv = styled.div`
   justify-content: space-between;
   @media screen and (max-width: 768px) {
     flex-direction: column;
-    height: 200px;
+    height: 220px;
     //border: 1px solid green;
   }
 `;
