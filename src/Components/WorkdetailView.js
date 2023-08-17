da62686073b8921e1bac85301a5e9a42e40680b2
hyein <rin1401@naver.com>
2023-08-17 19:50:21 +0900

    video test

--- src/Components/WorkdetailView.js
+++ src/Components/WorkdetailView.js
@@ -183,8 +183,8 @@ const WorkdetailView = ({ toggleModal, work }) => {
               <span>{work.detail[0]}</span>
             </InfoBox>
             <VideoBox>
-              <video width="100%" autoPlay loop playsInline muted>
-                <source src={work.imgs[1]} />
+              <video width="100%" height="100%" autoPlay loop playsInline muted>
+                <source src={work.imgs[1]} type="video/mp4" />
               </video>
             </VideoBox>
             <InfoBox>
@@ -192,8 +192,8 @@ const WorkdetailView = ({ toggleModal, work }) => {
               <span>{work.detail[2]}</span>
             </InfoBox>
             <VideoBox>
-              <video width="100%" autoPlay loop playsInline muted>
-                <source src={work.imgs[2]} />
+              <video width="100%" height="100%" autoPlay loop playsInline muted>
+                <source src={work.imgs[2]} type="video/mp4" />
               </video>
             </VideoBox>
             <InfoBox>
