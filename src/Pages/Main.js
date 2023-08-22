00ca288830ddbc4c437e3449dd435d4da8fc63ee
hyein <rin1401@naver.com>
2023-08-22 20:23:53 +0900

    animation

--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -10,6 +10,26 @@ const Container = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  position: relative;
+`;
+const Svg = styled.svg`
+  width: 100px;
+  height: 100px;
+  path {
+    stroke: #333;
+    stroke-width: 3px;
+  }
+`;
+const LogoBg = styled(motion.div)`
+  width: 100vw;
+  height: 100vh;
+  background-color: #ddd;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 1;
 `;
 const TitleOne = styled(motion.div)`
   display: flex;
@@ -51,6 +71,13 @@ const SubText = styled(motion.div)`
   }
 `;
 
+const svg = {
+  start: { fill: "rgba(51,51,51,0)", pathLength: 0 },
+  end: {
+    fill: "rgba(51,51,51,1)",
+    pathLength: 1,
+  },
+};
 const titleVariant = {
   start: {
     opacity: 0,
@@ -93,6 +120,23 @@ const Main = () => {
 
   return (
     <Container>
+      <Svg
+        xmlns="http://www.w3.org/2000/svg"
+        height="1em"
+        viewBox="0 0 448 512"
+      >
+        <motion.path
+          variants={svg}
+          initial="start"
+          animate="end"
+          transition={{
+            default: { duration: 10 },
+            fill: { duration: 1, delay: 1.8 },
+          }}
+          d="M288.88,76.48C172.7,76.48,78.17,171,78.17,287.19S172.7,497.91,288.88,497.91,499.6,403.38,499.6,287.19,405.07,76.48,288.88,76.48ZM103.17,287.19a184.91,184.91,0,0,1,43.89-119.75V348.51h27.2V251.58H277.67V437.2h27.2V355h37l55.85,82.17h.48a184.68,184.68,0,0,1-109.35,35.71C186.48,472.91,103.17,389.6,103.17,287.19ZM305.62,331V249.42H352.9q18.23,0,29.37,11.14t11.14,29.36q0,18.52-11.14,29.81T352.9,331ZM419,419.54l-46.17-68q22.86-6.94,35.45-23.44a60.8,60.8,0,0,0,12.58-37.91q0-19.09-8.24-33.85A57.43,57.43,0,0,0,389.5,233.5q-14.91-8.1-34.29-8.1H305.62V136.71H278.13V225.4h-.46v2.45H174.26V141.17a184.81,184.81,0,0,1,114.62-39.69c102.41,0,185.72,83.31,185.72,185.71A185.18,185.18,0,0,1,419,419.54Z"
+          transform="translate(-78.17 -76.48)"
+        />
+      </Svg>
       <TitleOne variants={titleVariant} initial="start" whileInView="end">
         {Array.from(title).map((t) => (
           <span>
@@ -110,6 +154,7 @@ const Main = () => {
             repeatType: "loop",
             duration: 20,
             repeat: Infinity,
+            delay: 3,
           }}
         >
           <span>
